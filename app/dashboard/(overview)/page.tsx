a8be305fc2c09df08eb54059478aad0e9c9d210e
chewqiutrain <leeyingqiu@gmail.com>
2024-05-01 15:55:23 +0800

    streaming + fallback for page

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,7 +1,7 @@
-import { Card } from "../ui/dashboard/cards";
-import RevenueChart from "../ui/dashboard/revenue-chart";
-import { lusitana } from "../ui/fonts";
-import LatestInvoices from "../ui/dashboard/latest-invoices";
+import { Card } from "@/app/ui/dashboard/cards";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import { lusitana } from "@/app/ui/fonts";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 
 

75eb394478a71791b099f1305068f3b1bfc9f7eb
chewqiutrain <leeyingqiu@gmail.com>
2024-05-01 17:09:05 +0800

    search and pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,16 +2,19 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useRouter, usePathname, useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
     console.log(`handleSearch: term ${term}`);
     // URLSearchParams: Web API that provides utility methods for manipulating the URL query parameters
     const params = new URLSearchParams(searchParams); 
+    params.set('query', "1");
     
     if (term) {
       params.set('query', term);
@@ -22,7 +25,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     const newPathname = `${pathname}?${params.toString()}`
     console.log(`newPathname: ${newPathname}`)
     replace(newPathname);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
